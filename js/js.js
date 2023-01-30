f2cfcd77078179dac855ea9cee585f8da3af3b27
Joe Quintero <joedarwin1998@gmail.com>
2023-01-30 11:43:26 -0400

    Calculo de cargar Bs a reserve

--- js/js.js
+++ js/js.js
@@ -1,8 +1,16 @@
 
-function enviar(){
+function RetirarPanama(){
     let monto=document.getElementById("recibe").value;
     let tasaDolar=document.getElementById("tasaDolar").value;
     let enviar = (monto*1)/tasaDolar;
     let redondeo = enviar.toFixed(2);
-    alert(redondeo);
+    return alert(redondeo);
+}
+
+function CargarBs(){
+    let TasaBs=document.getElementById("tasaBs").value;
+    let tienes=document.getElementById("tienesBs").value;
+    let recepcion = (tienes/TasaBs);
+    let redondeo = recepcion.toFixed(2);
+    return alert ("Serian: $"+redondeo);
 }
